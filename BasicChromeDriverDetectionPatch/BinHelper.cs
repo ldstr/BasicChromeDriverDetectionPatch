c97138c78b221c512dd534a9ca88be0c3bcc10d7
Plot <49232758+Plot1337@users.noreply.github.com>
2022-02-06 21:05:34 +1000

    Add files via upload

---
+++ BasicChromeDriverDetectionPatch/BinHelper.cs
@@ -0,0 +1,58 @@
+﻿namespace BasicChromeDriverDetectionPatch
+{
+    internal class BinHelper
+    {
+        public static List<int> FindOffsets(byte[]? source, byte[] find)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+
+            var result = new List<int>();
+            int matchIndex = 0;
+
+            for (int i = 0; i < source.Length; i++)
+            {
+                if (source[i] == find[matchIndex])
+                {
+                    if (matchIndex == (find.Length - 1))
+                    {
+                        result.Add(i - matchIndex);
+                        continue;
+                    }
+
+                    matchIndex++;
+                }
+                else if (source[i] == find[0]) matchIndex = 1;
+                else matchIndex = 0;
+            }
+
+            return result;
+        }
+
+        public static byte[] Replace(byte[] source, byte[] search, byte[] replace)
+        {
+            byte[]? dst = null;
+            byte[]? temp = null;
+
+            var offsets = FindOffsets(source, search);
+
+            foreach (var index in offsets)
+            {
+                temp = temp == null ? source : (dst ?? source);
+                dst = new byte[temp.Length - search.Length + replace.Length];
+
+                Buffer.BlockCopy(temp, 0, dst, 0, index);
+                Buffer.BlockCopy(replace, 0, dst, index, replace.Length);
+
+                Buffer.BlockCopy(
+                    temp,
+                    index + search.Length,
+                    dst,
+                    index + replace.Length,
+                    temp.Length - (index + search.Length));
+            }
+
+            return dst ?? source;
+        }
+    }
+}
---
+++ BasicChromeDriverDetectionPatch/Program.cs
@@ -0,0 +1,119 @@
+﻿using System.Text;
+
+namespace BasicChromeDriverDetectionPatch
+{
+    internal class Program
+    {
+        static byte[]? Data;
+
+        static void Main(string[] args)
+        {
+            Console.Title =
+                "Basic ChromeDriver Detection Patch - Plot <github.com/Plot1337>";
+
+            if (args.Length == 0)
+                ErrorExit(
+                    "Usage: [current exe] [path to chromedriver.exe]"
+                    );
+
+            string path = args[0];
+
+            try
+            {
+                Console.WriteLine($"Attempting to load... ({path})");
+                Data = File.ReadAllBytes(path);
+
+                Console.WriteLine("Loaded EXE bin into memory!");
+
+                var ids = GetCdcIds();
+
+                if (ids.Count == 0)
+                    ErrorExit("No IDs found! Perhaps the EXE is already patched?");
+
+                Console.WriteLine($"Found {ids.Count} IDs! Replacing with alts...");
+
+                byte[] AsciiStr(string str) =>
+                    Encoding.ASCII.GetBytes(str);
+
+                foreach (var id in ids)
+                {
+                    string alt = RandomString();
+
+                    Data = BinHelper.Replace(
+                        Data,
+                        AsciiStr(id),
+                        AsciiStr(alt)
+                        );
+
+                    Console.WriteLine($"{id} -> {alt}");
+                }
+
+                string patchedFilePath = path.Replace(".exe", "_patched.exe");
+
+                File.WriteAllBytes(patchedFilePath, Data);
+                Console.WriteLine("Saved to: " + patchedFilePath);
+            }
+            catch (FileNotFoundException) { ErrorExit("Chromedriver EXE not found!"); }
+            catch (Exception ex) { ErrorExit(ex.ToString()); }
+
+            Exit();
+        }
+
+        static List<string> GetCdcIds()
+        {
+            Console.WriteLine("Finding CDC ids...");
+
+            if (Data == null)
+                throw new ArgumentNullException(nameof(Data));
+
+            int idLen = 26;
+            var result = new List<string>();
+
+            // "cdc_" as bytes
+            byte[] searchArr = {
+                0x63, 0x64, 0x63, 0x5F
+            };
+
+            var offsets = BinHelper.FindOffsets(Data, searchArr);
+
+            foreach (var offset in offsets)
+            {
+                var idBytes = Data.Skip(offset).Take(idLen).ToArray();
+                string id = Encoding.UTF8.GetString(idBytes);
+
+                result.Add(id);
+                Console.WriteLine($"{offset}: {id}");
+            }
+
+            return result.Distinct().ToList();
+        }
+
+        static string RandomString(int length = 26)
+        {
+            var random = new Random();
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+            return new string(
+                Enumerable.Repeat(chars, length).Select(
+                    i => i[random.Next(i.Length)]
+                    ).ToArray()
+                );
+        }
+
+        static void ErrorExit(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Exit();
+        }
+
+        static void Exit()
+        {
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey(true);
+            Environment.Exit(0);
+        }
+    }
+}
